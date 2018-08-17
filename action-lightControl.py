0ebfbaf8bccc2173aa4177b46a512f32d2ed0055
jkuts <jubzimay@gmail.com>
2018-08-17 17:48:42 +0900

    changed tts line

--- action-lightControl.py
+++ action-lightControl.py
@@ -6,14 +6,17 @@ MQTT_PORT = 1883
 MQTT_ADDR = "{}:{}".format(MQTT_IP_ADDR, str(MQTT_PORT))
 
 def intent_received(hermes, intent_message):
+    sentence = 'You asked to '
 
-	sentence = 'what the hell is going on'
+    if intent_message.intent.intent_name == 'TurnAllLightsOn':
+        print('searchWeatherForecast')
+        sentence += 'turn the lights on '
+    
+    else:
+        return
 
-
-
-
-	hermes.publish_end_session(intent_message.session_id, sentence)
+    hermes.publish_end_session(intent_message.session_id, sentence)
 
 
 with Hermes(MQTT_ADDR) as h:
-    h.subscribe_intents(intent_received).start(
+    h.subscribe_intents(intent_received).start()    
